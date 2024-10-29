c46eaca09319766cd252356755a2cada7c4b1a67
laptop <laptop@DESKTOP-9427AUA>
2024-10-29 11:37:40 +0000

    Добавьте файлы проекта.

---
+++ saper_22/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace saper_22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(43, 64);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(104, 22);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(43, 96);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(104, 22);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 98);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(43, 148);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(104, 22);
+            this.numericUpDown3.TabIndex = 6;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 150);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "M";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(167, 7);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(995, 303);
+            this.panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1505, 651);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ saper_22/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saper_22
+{
+    public partial class Form1 : Form
+    {
+        byte i, j, k, x, y, x1, y1;
+        Random rnd = new Random();
+        bool f, flag;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Button[,] b;
+        byte[,] w;
+        bool[,] q;
+
+       
+
+        private void b_Click(object sender, MouseEventArgs e)
+        {  if (BackColor == Color.Red) return;
+            for (i = 0; i < x; i++)
+            {
+                for (j = 0; j < y; j++)
+                {
+                    if (sender == b[i, j])
+                    {   
+                        if (e.Button == MouseButtons.Right)
+                        {
+                            b[i, j].Text = (b[i, j].Text == "") ? "F" : "";
+                            return;
+                        }
+                        if (b[i, j].Text == "F") return;
+                        b[i, j].Enabled = false; b[i, j].BackColor = Color.White;
+                        if (w[i, j] == 10) { BackColor = Color.Red; return; }
+                        if (w[i,j]>0) { b[i, j].Text = "" + w[i, j]; return; }
+                        pustota(i,j); 
+                        return;
+                    }
+                }
+            }
+        }
+
+        void pustota(byte i8, byte j8)
+        {
+            if (j8 > 0) {   // up
+                for (j = (byte)(j8 - 1); j >=0 && j<y; j--)
+                {
+                    b[i8, j].Enabled = false; b[i8, j].BackColor = Color.White;
+                    if (w[i8, j] > 0) { b[i8, j].Text = "" + w[i8, j]; break; }
+                    else q[i8, j] = true;
+                }
+             }
+            if (j8 <y-1)  //down
+            {
+                for (j = (byte)(j8 + 1); j <y; j++)
+                {
+                    b[i8, j].Enabled = false; b[i8, j].BackColor = Color.White;
+                    if (w[i8, j] > 0) { b[i8, j].Text = "" + w[i8, j]; break; }
+                    else q[i8, j] = true;
+                }
+            }
+            if (i8 > 0)
+            {   // left
+                for (i = (byte)(i8 - 1); i >=0; i--)
+                {
+                    b[i, j8].Enabled = false; b[i, j8].BackColor = Color.White;
+                    if (w[i, j8] > 0) { b[i, j8].Text = "" + w[i, j8]; break; }
+                    else q[i, j8] = true;
+                }
+            }
+            if (i8 <x-1)
+            {   // right
+                for (i = (byte)(i8 + 1); i <x; i++)
+                {
+                    b[i, j8].Enabled = false; b[i, j8].BackColor = Color.White;
+                    if (w[i, j8] > 0) { b[i, j8].Text = "" + w[i, j8]; break; }
+                    else q[i, j8] = true;
+                }
+            }
+            if (i8 < x - 1 && j8>0)
+            {   // right-up
+                for (i = 1; i < 255; i++)
+                {   if (i8 + i > x - 1 || j8 - i < 0) break;
+                    b[i8+i, j8-i].Enabled = false; b[i8+i, j8-i].BackColor = Color.White;
+                    if (w[i8+i, j8-i] > 0) { b[i8+i, j8-i].Text = "" + w[i8+i, j8-i]; break; }
+                    else q[i8+i, j8-i] = true;
+                }
+            }
+            if (i8 >0 && j8 <y-1)
+            {   // left-down
+                for (i = 1; i < 255; i++)
+                {
+                    if (i8 - i <0 || j8 + i >y-1) break;
+                    b[i8 - i, j8 + i].Enabled = false; b[i8 - i, j8 + i].BackColor = Color.White;
+                    if (w[i8 - i, j8 + i] > 0) { b[i8 - i, j8 + i].Text = "" + w[i8 - i, j8 + i]; break; }
+                    else q[i8 - i, j8 + i] = true;
+                }
+            }
+            if (i8 > 0 && j8 < y - 1)
+            {   // left-up
+                for (i = 1; i < 255; i++)
+                {
+                    if (i8 - i <0  || j8-i<0) break;
+                    b[i8 - i, j8 - i].Enabled = false; b[i8 - i, j8 - i].BackColor = Color.White;
+                    if (w[i8 - i, j8 - i] > 0) { b[i8 - i, j8 - i].Text = "" + w[i8 - i, j8 - i]; break; }
+                    else q[i8 - i, j8 - i] = true;
+                }
+            }
+        }
+
+      
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.White;
+            panel1.Visible = false;
+            Application.DoEvents();
+            panel1.Controls.Clear();
+            b = null; w = null; q = null;
+            x = (byte)numericUpDown1.Value; y = (byte)numericUpDown2.Value;
+            b = new Button[x, y];  w = new byte[x, y]; q = new bool[x, y];
+            panel1.Width = 10 + x * 21; panel1.Height = 10 + y * 21;
+            for (i = 0; i < x; i++)
+            {
+                for (j = 0; j < y; j++)
+                {
+                    b[i, j] = new Button(); b[i, j].BackColor = Color.Yellow;
+                    b[i, j].SetBounds(5 + i * 21, 5 + j * 21, 20, 20);
+                    panel1.Controls.Add(b[i, j]);
+                    b[i, j].MouseDown += b_Click;
+                   
+                    w[i, j] = 0; q[i, j] = false;
+                }
+            }
+
+            for (i = 0; i < x * y * (byte)numericUpDown3.Value / 100; i++)
+            {
+               A1:  x1 = (byte)rnd.Next(x); y1 = (byte)rnd.Next(y);
+                if (w[x1, y1] == 10) goto A1;
+                w[x1, y1] = 10;
+               
+                if (x1 < x - 1 && y1 > 0 && w[x1 + 1, y1 - 1] < 10) w[x1 + 1, y1 - 1]++;
+                if (x1 < x - 1  && w[x1 + 1, y1 ] < 10) w[x1 + 1, y1 ]++;
+                if (x1 < x - 1 && y1 <y-1 && w[x1 + 1, y1 + 1] < 10) w[x1 + 1, y1 + 1]++;
+
+                if (y1 < y - 1 && w[x1, y1 + 1] < 10) w[x1, y1 + 1]++;
+                if (x1 >0 && y1 < y - 1 && w[x1 - 1, y1 + 1] < 10) w[x1 - 1, y1 + 1]++;
+                if (x1 >0 &&  w[x1 - 1, y1 ] < 10) w[x1 - 1, y1]++;
+                if (x1 >0 && y1 >0 && w[x1 - 1, y1 - 1] < 10) w[x1 - 1, y1 - 1]++;
+                if (y1 >0 && w[x1, y1 - 1] < 10) w[x1 , y1 - 1]++;
+
+            }
+
+            panel1.Visible = true;
+
+        }
+
+       
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
